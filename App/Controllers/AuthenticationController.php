a5be82f38de6503ab8cebc11289ff3b83f2199fa
Brayan Vargas <103152474+BrayanAntonio08@users.noreply.github.com>
2023-12-02 12:39:21 -0600

    Corrección login

--- App/Controllers/AuthenticationController.php
+++ App/Controllers/AuthenticationController.php
@@ -1,38 +1,41 @@
 <?php
-require_once(MODEL_PATH."User.model.php");
-    class AuthenticationController extends Controller {
-        
-        public function login(){
+require_once(MODEL_PATH . "User.model.php");
+class AuthenticationController extends Controller
+{
 
-                       $this->render('none');
+    public function login()
+    {
+
+        $this->render('none');
+    }
+    public function logUser()
+    {
+        $userModel = new UserModel();
+
+        // Obtener todos los usuarios desde la base de datos
+        $allUsers = $userModel->getAllUsers();
+
+        // Suponiendo que has recibido los valores del formulario
+        $formUsername = $_POST['username'];
+        $formPassword = $_POST['password'];
+        $encontrado = false;
+        // Iterar a trav�s de los usuarios y verificar si coinciden los valores del formulario
+        foreach ($allUsers as $user) {
+            if ($user['nombre_usuario'] === $formUsername && $user['contrasenia'] === $formPassword) {
+                $encontrado = true;
+
+            }
         }
-        public function logUser(){
-                        $userModel = new UserModel();
+        if ($encontrado) {
+            session_start();
+            $_SESSION['user'] = $formUsername;
+            //    echo "Nombre de usuario o contrase�a correctos.";
+            $this->redirect('/Page/Home');
 
-// Obtener todos los usuarios desde la base de datos
-$allUsers = $userModel->getAllUsers();
+        } else {
+            $info['mensaje'] = 'Datos incorrectos';
+            $this->redirect('/Authentication/login', $info);
+        }
 
-// Suponiendo que has recibido los valores del formulario
-$formUsername = $_POST['username'];
-$formPassword = $_POST['password'];
-$encontrado=false;
-// Iterar a trav�s de los usuarios y verificar si coinciden los valores del formulario
-foreach ($allUsers as $user) {
-    if ($user['nombre_usuario'] === $formUsername && $user['contrasenia'] === $formPassword) {
-        $encontrado=true;
-        
     }
 }
-if($encontrado){
-             session_start();
-         $_SESSION['user'] = $formUsername;
-        //    echo "Nombre de usuario o contrase�a correctos.";
-              $this->redirect('/Page/Home');
-
-} else {
-    $info['mensaje'] = 'Datos incorrectos';
-                         $this->redirect('/Authentication/login', $info);
-}
-
-        }
-    }
